05d4539e2c1f0e560f2bf8094b3da153c4e5b22c
Dominic Kirste <dominic.kirste@deutschebahn.com>
2023-01-26 23:43:21 +0100

    core: add config for unocss

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from '@unocss/vite'
-import presetMini from '@unocss/preset-mini'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    UnoCSS({
-      presets: [presetMini({})],
-    }),
-  ],
+  plugins: [vue(), UnoCSS()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
