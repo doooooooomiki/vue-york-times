51c29e84832f6bab6dd1e7a2dab46a3058b5a198
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-07-12 09:20:54 +0200

    ✨ deps: install tailwindcss
    https://tailwindcss.com/docs/installation/framework-guides/nuxt
    https://ui.nuxt.com/getting-started/installation/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
@@ -8,5 +10,15 @@ export default defineNuxtConfig({
     compatibilityVersion: 4,
   },
 
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  css: ['~/assets/css/main.css'],
+
   modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui'],
+
+  ui: {
+    fonts: false,
+  },
 });
